ab4985f404ed403ed14f4e602e72aab947a960a7
WONG, Sai Lung <marco.coolcat55@gmail.com>
2024-10-19 22:13:10 +0800

    function isEnd()

--- Gomoku.java
+++ Gomoku.java
@@ -18,20 +18,21 @@ public class Gomoku {
     }
 
     public void gameStart() {
-        int x, y, player = 1;
+        int x = 0, y = 0, player = 2;
+        printBoard();
         // gameInit();
-        while (true) {
-            clearPrint();
-            printBoard();
-            if (isEnd()) return;
+        do {
+            player = 3 - player;
             do {
                 printInfo(player);
                 x = sc.nextInt();
                 y = sc.nextInt();
             } while (!isInRange(x, y) || !isVaild(x, y));
             placePiece(player, x, y);
-            player = 3 - player;
-        }
+            clearPrint();
+            printBoard();
+        } while (!isEnd(x, y));
+        System.out.println("Player " + player + " wins!");
     }
 
     public void gameInit() {
@@ -73,18 +74,93 @@ public class Gomoku {
         board[x][y] = player;
     }
 
-    public boolean isEnd() {
-        
-        return false;
+    public boolean isEnd(int x, int y) {
+        return (
+            isVerticalSame(x, y, 5) ||
+            isHorizontalSame(x, y, 5)||
+            isDiagonalUL2DRSame(x, y, 5)||
+            isDiagonalDL2URSame(x, y, 5));
+    }
+
+    public boolean isHorizontalSame(int x, int y, int noOfSame) {
+        int n = 1;
+
+        int i = 0;
+        while (
+            n < noOfSame &&
+            y + i + 1 < bLen &&
+            board[x][y + i] == board[x][y + i++ + 1]
+        ) ++n;
+
+        i = 0;
+        while (
+            n < noOfSame &&
+            y - i - 1 >= 0 &&
+            board[x][y - 1] == board[x][y - i++ - 1]
+        ) ++n;
+
+        return n >= noOfSame;
+    }
+
+    public boolean isVerticalSame(int x, int y, int noOfSame) {
+        int n = 1;
+
+        int i = 0;
+        while (
+            n < noOfSame &&
+            x + i + 1 < bLen &&
+            board[x + i][y] == board[x + i++ + 1][y]
+        ) ++n;
+
+        i = 0;
+        while (
+            n < noOfSame &&
+            x - i - 1 >= 0 &&
+            board[x - i][y] == board[x - i++ - 1][y]
+        ) ++n;
+
+        return n >= noOfSame;
+    }
+
+    public boolean isDiagonalUL2DRSame(int x, int y, int noOfSame) {
+        int n = 1;
+        return n >= noOfSame;
+    }
+
+    public boolean isDiagonalDL2URSame(int x, int y, int noOfSame) {
+        int n = 1;
+        return n >= noOfSame;
     }
 
     public static void clearPrint() {
         System.out.print("\033[H\033[2J");
     }
     public static void main(String[] args) {
+        clearPrint();
         Gomoku myGomokuGame = new Gomoku(10);
         // myGomokuGame.printBoard();
         myGomokuGame.gameStart();
         // System.out.println(myGomokuGame.isInRange(0, 0));
     }
 }
+/*
+0 0
+0 1
+1 0
+1 1
+2 0
+2 1
+3 0
+3 1
+4 0
+
+0 0
+1 0
+0 1
+1 1
+0 2
+1 2
+0 3
+1 3
+0 4
+ */
